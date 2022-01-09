1aedee80c97bc7472053527435ec43b6b675a96f
jinwoo park <swat018@gmail.com>
2022-01-10 07:16:55 +0900

    Initial commit

---
+++ src/main/java/com/example/demobootoverriding/App.java
@@ -0,0 +1,40 @@
+package com.example.demobootoverriding;
+
+import com.example.demofamiliymanprep.FamilyMan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+
+            @Autowired FamilyMan familyMan;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                System.out.println("==================================");
+                System.out.println(familyMan.getName());
+                System.out.println("==================================");
+            }
+        };
+    }
+
+    @Bean
+    public FamilyMan familyMan() {
+        FamilyMan familyMan = new FamilyMan();
+        familyMan.setName("jinwoo");
+        return familyMan;
+    }
+
+}
